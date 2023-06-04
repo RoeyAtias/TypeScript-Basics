26c37f3e7ffd224e0896a6b1ee390c95c9da3eeb
רועי אטיאס <rwytys@h-MacBook-Pro-sl-rwy.local>
2023-06-04 21:07:06 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+// Variables
+
+let a: number;
+
+// a = 'hello' ERROR
+// a = false ERROR
+a = 200;
+
+let b: number | string | Date;
+
+b = 200;
+// b = false ERROR
+b = 'true'
+b = new Date();
+
+let c: any;
+
+c = true;
+c = 'hello';
+c = 145;
+
+let f;
+
+f = 'hello'
+f = 250;
+
+
+// -------------------------------
+
+function func(age: number, name: string) {
+    console.log(name + ' is ' + age + ' years old')
+}
+
+func(34, 'Yossi');
+
